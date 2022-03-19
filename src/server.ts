927a8cfb9115a7e307177eb068494010ae4d29f0
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2022-03-19 10:31:11 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import apiRoutes from './routes/api';
 
 dotenv.config();
 
 const server = express();
 
+server.use(cors());
+
 server.use(express.static(path.join(__dirname, '../public')));
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
+
+server.use('/api', apiRoutes);
 
 server.use((req: Request, res: Response) => {
-    res.status(404);
-    res.json({error: 'Endpoint não encontrado.'});
+    // res.status(404);
+    res.status(404).json({ error: 'Endpoint não encontrado.' });
 });
 
 server.listen(process.env.PORT);
